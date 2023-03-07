feb1d99e8aaade8e55e1ed5eb053217a5102684d
Sakshi Rawat <sakshirawat@Sakshis-MacBook-Air.local>
2023-03-07 13:13:32 +0530

    unique key error fixed in plans.js file

--- src/components/Plans.jsx
+++ src/components/Plans.jsx
@@ -15,23 +15,23 @@ const Plans = () => {
 
       {/* plans card */}
       <div className="plans">
-        {plansData.map((plan, i) => (
-          <div className="plan" key={i}>
+        {plansData.map((plan) => (
+          <div className="plan" key={plan.id}>
             {plan.icon}
             <span>{plan.name}</span>
             <span>₹ {plan.price}</span>
 
             <div className="features">
-              {plan.features.map((feature, i) => (
-                <div className="feature">
+              {plan.features.map((feature,id) => (
+                <div className="feature" key={id}>
                   <img src={whiteTick} alt="" />
-                  <span key={i}>{feature}</span>
+                  <span>{feature}</span>
                 </div>
               ))}
             </div>
 
             <div>
-              <span>See more benefits -> </span>
+              <span>See more benefits</span>
             </div>
             <button className="btn">Join now</button>
           </div>
